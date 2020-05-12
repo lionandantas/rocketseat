6b2c06624fe0447b9abc36fd2ad538e68be13de5
Lionan Dantas <=>
2020-05-12 09:06:58 -0300

    adc nivel 02

---
+++ nivel-02/src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+
+
+routes.get('/', (request, response) => {
+  return response.json({
+    message: 'Hellow'
+  })
+});
+
+
+export default routes;
